daab2be14560eb0e4e14c311731ec366aeb2ac57
Anson Miu <me@ansonmiu.dev>
2023-12-01 21:55:14 +0000

    Add day01 solution - Python

--- drivers/python/aoc/solutions/day01.py
+++ drivers/python/aoc/solutions/day01.py
@@ -2,26 +2,44 @@
 from aoc.helpers.runnerlib import run_solution
 
 
-def extract_calories(input: str) -> list[int]:
-    return [
-        sum(int(food.strip()) for food in elf.splitlines())
-        for elf in input.split("\n\n")
-    ]
+_SPELLING_TO_INT = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+def parse_digits(line: str) -> list[int]:
+    digits: list[int] = []
+    for idx in range(len(line)):
+        if line[idx].isdigit():
+            digits.append(int(line[idx]))
+        else:
+            for spelling, number in _SPELLING_TO_INT.items():
+                if line[idx:].startswith(spelling):
+                    digits.append(number)
+                    break
+    return digits
+
+
+def parse_calibration_value(line: str) -> int:
+    digits = parse_digits(line)
+    return (digits[0] * 10) + digits[-1]
 
 
 def part1(input: str) -> int:
-    calories_by_elf = extract_calories(input)
-    return max(calories_by_elf)
+    return sum(map(parse_calibration_value, input.splitlines()))
 
 
 def part2(input: list[str]) -> int:
-    calories_by_elf = extract_calories(input)
-    return sum(sorted(calories_by_elf, reverse=True)[:3])
-
-
-def format_calories(calories: int) -> str:
-    return f"{calories:,} calories"
+    return sum(map(parse_calibration_value, input.splitlines()))
 
 
 if __name__ == "__main__":
-    run_solution(part1, part2, formatter=format_calories)
+    run_solution(part1, part2)
